d11d4c49cf1ca06b5d2a7d4fc4d4ebce9097ed83
Mathias B <mathias.buss@student.htw-berlin.de>
2011-12-18 22:27:39 +0100

    config ordner angelegt

---
+++ Programming/Programming/config/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programming
+{
+    class Config
+    {
+        public enum value : int
+        {
+            PIXEL_STATE_CONTAINER_AMOUNT = 5, THREADS = 5
+        };
+
+    }
+}
