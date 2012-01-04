3899e2d1f2397256f5d9eabe3e9581d01316c583
Mathias B <mathias.buss@student.htw-berlin.de>
2012-01-04 22:50:18 +0100

    ordner fuer exceptions zur besseren uebersicht angelegt

---
+++ Programming/Programming/exceptions/FilterUndefinedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programming
+{
+    class FilterUndefinedException : Exception
+    {
+        public FilterUndefinedException()
+        {
+        }
+
+        public FilterUndefinedException(string message)
+        : base(message)
+        {
+        }
+
+        public FilterUndefinedException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
